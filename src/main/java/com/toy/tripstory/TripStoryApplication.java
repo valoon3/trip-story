2e7caeca842a86ff85f16494159dc0f5aebefb50
valoon3 <valoon2@naver.com>
2024-12-10 19:54:44 +0900

    [feat] init

---
+++ src/main/java/com/toy/tripstory/TripStoryApplication.java
@@ -0,0 +1,13 @@
+package com.toy.tripstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/tripstory/TripStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.tripstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
