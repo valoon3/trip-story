1cc15e059bb614a570af287283247a2fa97c38be
valoon3 <valoon2@naver.com>
2024-12-10 23:03:39 +0900

    [feat] local db 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.jetbrains.kotlin.kapt") version "1.9.10" // Kotlin Kapt 플러그인
 }
 
 group = "com.toy"
@@ -28,10 +29,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
+
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/java/com/toy/tripstory/util/TestConfig.java
@@ -0,0 +1,28 @@
+package com.toy.tripstory.util;
+
+import jakarta.annotation.PostConstruct;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@NoArgsConstructor
+public class TestConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @PostConstruct
+    public void printProperties() {
+        log.info("DB URL: {}", dbUrl);
+        log.info("DB Username: {}", dbUsername);
+        log.info("DB Password: {}", dbPassword);
+    }
+}
